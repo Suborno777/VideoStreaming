6b5551c5345aec1c5dbf75f957a126d7619f9868
Suborno777 <127844788+Suborno777@users.noreply.github.com>
2023-05-08 16:43:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var users = [`Suborno`]
+
+const memberDiv = document.querySelector('.memberDiv');
+const addIcon = document.querySelector('.addIcon');
+
+const userIcons = () => {
+    users.reverse();
+    users.map((curElem) => {
+        memberDiv.insertAdjacentHTML('afterbegin', `
+        <button class="btn" onclick='transitionToPage("browse.html")'><span>${curElem}</span></button>
+        `);
+    })
+};
+
+addIcon.addEventListener('click', () => {
+    let user = prompt('please enter your name');
+
+    if(user != null && !users.includes(user)){
+        users.push(user);
+        console.log(users);
+        memberDiv.insertAdjacentHTML('afterbegin', `
+        <button class="btn"><span>${user}</span></button>
+        `);
+    }else{
+        alert('username already exist');
+    }
+})
+
+
+userIcons();
+
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
+}
